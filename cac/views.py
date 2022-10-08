53b9c4370a9125d5d9f431991a4d10b47cc2a017
unknown <gisecy@gmail.com>
2022-10-08 12:12:59 -0300

    Se agrega archivo urls.py en app cac

--- cac/views.py
+++ cac/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
+def hola_mundo(request):
+    return HttpResponse("Hola mundo Django")
+    
